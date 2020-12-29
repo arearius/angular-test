c635259e911b324cb7a311197a5ba46b095b8f3b
arearius <arearius@gmail.com>
2020-12-29 22:39:37 +0300

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, VERSION } from '@angular/core';
+import {NgbDateStruct, NgbCalendar} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'my-app',
@@ -6,10 +7,20 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
+  
+  constructor(private calendar: NgbCalendar) {
+    
+  }
+
   name = 'Angular ' + VERSION.major;
   model = {
     left: true,
     middle: false,
     right: false
   };
+  date: {year: number, month: number};
+  model_calendar: NgbDateStruct;
+  selectToday() {
+    this.model_calendar = this.calendar.getToday();
+  }
 }
